fde8ee01db3ac970e38db3f93f9726ee38f09d70
gustcode <73033092+gustcode@users.noreply.github.com>
2022-03-06 20:58:42 +0700

    added inbox command

--- gwen.py
+++ gwen.py
@@ -224,11 +224,8 @@ async def help(ctx):
 
 @client.command()
 async def inbox(ctx):
-    em = nextcord.Embed(title=f"Update {versionbot}", colour=nextcord.Color.from_rgb(87, 7, 0))
-    em.add_field(name="fixed issue where member gets timeout for nothing.", inline=False)
-    em.add_field(name="mod commands are now guild only.", inline=False)
-    em.add_field(name="added 20s timeout for coinflips.", inline=False)
-    em.add_field(name="help commands are now sent via DMs.", inline=False)
+    em = nextcord.Embed(title=f"Update v{versionbot}", colour=nextcord.Color.from_rgb(87, 7, 0))
+    em.add_field(name=f"v{versionbot}", value="- fixed issue where member gets timeout for nothing.\n\nmod commands are now guild only.\n\nadded 20s timeout for coinflips.\n\nhelp commands are now sent via DMs.\n\n", inline=False)
     await ctx.send(embed=em)
 
 client.run(os.getenv('TOKEN'))
