6eb36a255936504fffca791cb11f16b6edb679b3
gustcode <73033092+gustcode@users.noreply.github.com>
2022-03-06 19:46:12 +0700

    make the mod command guild only

--- cogs/mod.py
+++ cogs/mod.py
@@ -9,6 +9,7 @@ class Moderation(commands.Cog):
 
     #command to kick a member
     @commands.command()
+    @commands.guild_only()
     @commands.has_permissions(kick_members=True) #checking if the caller has kick permissions
     async def kick(self, ctx, member : nextcord.Member, *, reason=None):
         await member.kick(reason=reason)
@@ -20,10 +21,13 @@ class Moderation(commands.Cog):
         if isinstance(error, commands.MissingPermissions):
             await ctx.reply("you don't have permission to kick members, dumbass.") 
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.reply("you forgot to mention someone.")           
+            await ctx.reply("you forgot to mention someone.")
+        if isinstance(error, commands.BadArgument):
+            await ctx.reply("sorry, there has been an error.")           
     
     #command to ban a member   
     @commands.command()
+    @commands.guild_only()
     @commands.has_permissions(ban_members=True) #checking if the caller has ban permissions
     async def ban(self, ctx, member : nextcord.Member, *, reason=None):
         await member.ban(reason=reason)
@@ -35,10 +39,13 @@ class Moderation(commands.Cog):
         if isinstance(error, commands.MissingPermissions):
             await ctx.reply("you don't have permission to ban members, dumbass.")    
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.reply("you forgot to mention someone.")   
+            await ctx.reply("you forgot to mention someone.")
+        if isinstance(error, commands.BadArgument):
+            await ctx.reply("sorry, there has been an error.")   
 
     #command to unban a member
     @commands.command()
+    @commands.guild_only()
     @commands.has_permissions(ban_members=True) #checking if the caller has ban permissions
     async def unban(self, ctx, *, member): 
         banned_users = await ctx.guild.bans()
@@ -57,9 +64,12 @@ class Moderation(commands.Cog):
     async def unban_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
             await ctx.reply("you don't have permission to ban members, dumbass.")
+        if isinstance(error, commands.BadArgument):
+            await ctx.reply("sorry, there has been an error.")
             
     #command a put a user in timeout
     @commands.command()
+    @commands.guild_only()
     @commands.has_permissions(manage_messages=True)
     async def timeout(self, ctx, member : nextcord.Member, time, *, reason=None):
         time = humanfriendly.parse_timespan(time)
@@ -73,9 +83,12 @@ class Moderation(commands.Cog):
             await ctx.reply("you don't have permissions to timeout members, shithead.") 
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.reply("please type the command correctly $timeout @target time(m/s) reason(optional).")
-        
+        if isinstance(error, commands.BadArgument):
+            await ctx.reply("sorry, there has been an error.")
+
     #command to untimeout a user
     @commands.command()
+    @commands.guild_only()
     @commands.has_permissions(manage_messages=True)
     async def untimeout(self, ctx, member : nextcord.Member):
         await member.edit(timeout=None)
@@ -88,6 +101,8 @@ class Moderation(commands.Cog):
             await ctx.reply("you don't have permissions to timeout members, shithead.")
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.reply("please type the command correctly $untimeout @target.")
+        if isinstance(error, commands.BadArgument):
+            await ctx.reply("sorry, there has been an error.")
 
 def setup(client):
     client.add_cog(Moderation(client))
